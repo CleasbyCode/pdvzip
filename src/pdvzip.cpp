bce5f186484d90eba57d55d8db4e36d40263d391
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2023-01-05 22:33:57 +0000

    Update pdvzip.cpp

--- src/pdvzip.cpp
+++ src/pdvzip.cpp
@@ -275,7 +275,7 @@ void eraseChunks(std::vector<unsigned char>& ImageVec) {
 
 	// Remove chunks. 
 	while (chunk--) {
-		const ptrdiff_t CHUNK_INDEX = search(ImageVec.begin(), ImageVec.end(), CHUNKS_TO_REMOVE[chunk].begin(), CHUNKS_TO_REMOVE[chunk].end()) - ImageVec.begin() - 4;
+		const ptrdiff_t CHUNK_INDEX = search(ImageVec.begin(), ImageVec.end(), CHUNKS_TO_REMOVE[chunk].begin(), CHUNKS_TO_REMOVE[chunk].end())-ImageVec.begin()-4;
 		if (firstIdatIndex > CHUNK_INDEX) {
 			int chunkLength = (ImageVec[CHUNK_INDEX + 2] << 8) | ImageVec[CHUNK_INDEX + 3];
 			ImageVec.erase(ImageVec.begin() + CHUNK_INDEX, ImageVec.begin() + CHUNK_INDEX + (chunkLength + 12));
