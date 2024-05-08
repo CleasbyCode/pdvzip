78c1798af42867ba674a04e33c6d25fa2fd3ca91
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-08 14:35:43 +0100

    Update pdvzip.cpp
    pdvzip 2.3.1
    bug fixes and code refactor.

--- src/pdvzip.cpp
+++ src/pdvzip.cpp
@@ -7,12 +7,13 @@
 // 	Run it:
 // 	$ ./pdvzip
 
-#include <algorithm>
-#include <fstream>
-#include <iostream>
-#include <regex>
-#include <string>
 #include <vector>
+#include <unordered_map>
+#include <string>
+#include <regex>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
 
 using uchar = unsigned char;
 
@@ -348,6 +349,7 @@ void startPdv(const std::string& IMAGE_NAME, const std::string& ZIP_NAME, bool i
 	std::vector<uchar> Iccp_Script_Vec = { 0x00, 0x00, 0x00, 0x00, 0x69, 0x43, 0x43, 0x50, 0x44, 0x56, 0x5A, 0x49, 0x50, 0x5F, 0x5F, 0x00, 0x00, 0x0D, 0x52, 0x45, 0x4D, 0x3B, 0x0D, 0x0A, 0x00, 0x00, 0x00, 0x00 };
 
 	std::vector<std::vector<uchar>> Extraction_Scripts_Vec { 
+		
 		{	0x49, 0x54, 0x45, 0x4D, 0x3D, 0x22, 0x22, 0x3B, 0x44, 0x49, 0x52, 0x3D, 0x22, 0x70, 0x64, 0x76, 0x7A, 0x69, 0x70, 0x5F, 0x65, 0x78, 0x74, 0x72, 0x61, 0x63,
 			0x74, 0x65, 0x64, 0x22, 0x3B, 0x4E, 0x55, 0x4C, 0x3D, 0x22, 0x2F, 0x64, 0x65, 0x76, 0x2F, 0x6E, 0x75, 0x6C, 0x6C, 0x22, 0x3B, 0x63, 0x6C, 0x65, 0x61, 0x72,
 			0x3B, 0x6D, 0x6B, 0x64, 0x69, 0x72, 0x20, 0x2D, 0x70, 0x20, 0x22, 0x24, 0x44, 0x49, 0x52, 0x22, 0x3B, 0x6D, 0x76, 0x20, 0x22, 0x24, 0x30, 0x22, 0x20, 0x22,
@@ -506,50 +508,21 @@ void startPdv(const std::string& IMAGE_NAME, const std::string& ZIP_NAME, bool i
 			0x25, 0x7E, 0x78, 0x30, 0x22, 0x20, 0x2A, 0x2E, 0x70, 0x6E, 0x67, 0x26, 0x22, 0x22, 0x26, 0x65, 0x63, 0x68, 0x6F, 0x2E, 0x26, 0x65, 0x78, 0x69, 0x74 }
 	};
 
-	std::vector<int> Case_Values_Vec;
+	std::unordered_map<int, std::vector<int>> case_map = {
+		{VIDEO_AUDIO,		{0, 484, 28}},
+		{PDF,			{1, 406, 28}},
+		{PYTHON,		{2, 267, 257, 188, 28}},
+		{POWERSHELL,		{3, 261, 251, 182, 51}},
+		{BASH_SHELL,		{4, 308, 306, 142, 28}},
+		{WINDOWS_EXECUTABLE,	{5, 278, 276}},
+		{FOLDER,		{6, 329, 28}},
+		{LINUX_EXECUTABLE,	{7, 142, 28}},
+		{JAR,			{8}},
+		{-1,			{9, 295, 28}} // Default case
+	};
 
-	switch (extension_list_index) {
-		case VIDEO_AUDIO: {
-			Case_Values_Vec = { 0, 484, 28 };
-			break;
-		}
-		case PDF: {
-			Case_Values_Vec = { 1, 406, 28 };
-			break;
-		}
-		case PYTHON: {
-			Case_Values_Vec = { 2, 267, 257, 188, 28 };
-			break;
-		}
-		case POWERSHELL: {
-			Case_Values_Vec = { 3, 261, 251, 182, 51 };
-			break;
-		}
-		case BASH_SHELL: {
-			Case_Values_Vec = { 4, 308, 306, 142, 28 };
-			break;
-		}
-		case WINDOWS_EXECUTABLE: {
-			Case_Values_Vec = { 5, 278, 276 };
-			break;
-		}
-		case FOLDER: {
-			Case_Values_Vec = { 6, 329, 28 };
-			break;
-		}
-		case LINUX_EXECUTABLE: {
-			Case_Values_Vec = { 7, 142, 28 };
-			break;
-		}
-		case JAR: {
-			Case_Values_Vec = { 8 };
-			break;
-		}
-		default: {
-			Case_Values_Vec = { 9, 295, 28 };
-		}
-	}
-
+	std::vector<int> Case_Values_Vec = case_map.count(extension_list_index) ? case_map[extension_list_index] : case_map[-1];
+	
 	const int SCRIPT = Case_Values_Vec[0];
 
 	constexpr int SCRIPT_INSERT_INDEX = 22;
