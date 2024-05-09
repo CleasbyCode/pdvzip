5e988404430afbb4509f67130d46d54b5eb6b574
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-09 13:21:56 +0100

    Update fix_zip.cpp

--- src/fix_zip.cpp
+++ src/fix_zip.cpp
@@ -1,5 +1,4 @@
-#include <algorithm>
-#include <string>
+// ZIP file has been moved to another location. We need to find and adjust the ZIP file record offsets to their new location.
 
 void adjustZipOffsets(std::vector<unsigned char>& Image_Vec, const size_t LAST_IDAT_CHUNK_NAME_INDEX, bool isBigEndian) {
  
@@ -9,32 +8,33 @@ void adjustZipOffsets(std::vector<unsigned char>& Image_Vec, const size_t LAST_I
 		END_CENTRAL_DIR_SIG = "PK\x05\x06";
 
 	const size_t
-		START_CENTRAL_DIR_INDEX = std::search(Image_Vec.begin() + LAST_IDAT_CHUNK_NAME_INDEX, Image_Vec.end(), 		START_CENTRAL_DIR_SIG.begin(), START_CENTRAL_DIR_SIG.end()) - Image_Vec.begin(),
-	END_CENTRAL_DIR_INDEX = std::search(Image_Vec.begin() + START_CENTRAL_DIR_INDEX, Image_Vec.end(), END_CENTRAL_DIR_SIG.begin(), END_CENTRAL_DIR_SIG.end()) - Image_Vec.begin();
+		START_CENTRAL_DIR_INDEX = std::search(Image_Vec.begin() + LAST_IDAT_CHUNK_NAME_INDEX, Image_Vec.end(), 		
+		START_CENTRAL_DIR_SIG.begin(), START_CENTRAL_DIR_SIG.end()) - Image_Vec.begin(),
+		END_CENTRAL_DIR_INDEX = std::search(Image_Vec.begin() + START_CENTRAL_DIR_INDEX, Image_Vec.end(), END_CENTRAL_DIR_SIG.begin(), END_CENTRAL_DIR_SIG.end()) - Image_Vec.begin();
 
-size_t
-	zip_records_index = END_CENTRAL_DIR_INDEX + 11,
-	zip_comment_length_index = END_CENTRAL_DIR_INDEX + 21,
-	end_central_start_index = END_CENTRAL_DIR_INDEX + 19,
-	central_local_index = START_CENTRAL_DIR_INDEX - 1,
-	new_offset = LAST_IDAT_CHUNK_NAME_INDEX;
+	size_t
+		zip_records_index = END_CENTRAL_DIR_INDEX + 11,
+		zip_comment_length_index = END_CENTRAL_DIR_INDEX + 21,
+		end_central_start_index = END_CENTRAL_DIR_INDEX + 19,
+		central_local_index = START_CENTRAL_DIR_INDEX - 1,
+		new_offset = LAST_IDAT_CHUNK_NAME_INDEX;
 
-int zip_records = (Image_Vec[zip_records_index] << 8) | Image_Vec[zip_records_index - 1];
+	uint16_t zip_records = (Image_Vec[zip_records_index] << 8) | Image_Vec[zip_records_index - 1];
 
-isBigEndian = false;
-int value_bit_length = 32;
+	isBigEndian = false;
+	uint8_t value_bit_length = 32;
 
-while (zip_records--) {
-	new_offset = std::search(Image_Vec.begin() + new_offset + 1, Image_Vec.end(), ZIP_SIG.begin(), ZIP_SIG.end()) - Image_Vec.begin(),
-	central_local_index = 45 + std::search(Image_Vec.begin() + central_local_index, Image_Vec.end(), START_CENTRAL_DIR_SIG.begin(), START_CENTRAL_DIR_SIG.end()) - Image_Vec.begin();
-	valueUpdater(Image_Vec, central_local_index, new_offset, value_bit_length, isBigEndian);
-}
+	while (zip_records--) {
+		new_offset = std::search(Image_Vec.begin() + new_offset + 1, Image_Vec.end(), ZIP_SIG.begin(), ZIP_SIG.end()) - Image_Vec.begin(),
+		central_local_index = 45 + std::search(Image_Vec.begin() + central_local_index, Image_Vec.end(), START_CENTRAL_DIR_SIG.begin(), START_CENTRAL_DIR_SIG.end()) - Image_Vec.begin();
+		valueUpdater(Image_Vec, central_local_index, new_offset, value_bit_length, isBigEndian);
+	}
 
-valueUpdater(Image_Vec, end_central_start_index, START_CENTRAL_DIR_INDEX, value_bit_length, isBigEndian);
+	valueUpdater(Image_Vec, end_central_start_index, START_CENTRAL_DIR_INDEX, value_bit_length, isBigEndian);
 
-int zip_comment_length = 16 + (static_cast<size_t>(Image_Vec[zip_comment_length_index] << 8) | (static_cast<size_t>(Image_Vec[zip_comment_length_index - 1])));
+	uint16_t zip_comment_length = 16 + (static_cast<size_t>(Image_Vec[zip_comment_length_index] << 8) | (static_cast<size_t>(Image_Vec[zip_comment_length_index - 1])));
 
-value_bit_length = 16;
+	value_bit_length = 16;
 
-valueUpdater(Image_Vec, zip_comment_length_index, zip_comment_length, value_bit_length, isBigEndian);
+	valueUpdater(Image_Vec, zip_comment_length_index, zip_comment_length, value_bit_length, isBigEndian);
 }
