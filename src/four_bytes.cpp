b082c13372acffdd540f50cf363d29446b9b5f9c
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-10 12:59:34 +0100

    Update four_bytes.cpp

--- src/four_bytes.cpp
+++ src/four_bytes.cpp
@@ -1,6 +1,6 @@
-size_t getFourByteValue(const std::vector<uchar>& vec, size_t index) {
-	return	(static_cast<size_t>(vec[index]) << 24) |
-		(static_cast<size_t>(vec[index + 1]) << 16) |
-		(static_cast<size_t>(vec[index + 2]) << 8) |
-		static_cast<size_t>(vec[index + 3]); 
+size_t getFourByteValue(const std::vector<uchar>& VEC, const size_t INDEX) {
+	return	(static_cast<size_t>(VEC[INDEX]) << 24) |
+		(static_cast<size_t>(VEC[INDEX + 1]) << 16) |
+		(static_cast<size_t>(VEC[INDEX + 2]) << 8) |
+		static_cast<size_t>(VEC[INDEX + 3]); 
 }
