6dc3cd85abd57d01366d33e6e1315954ca3b0427
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-13 19:30:38 +0100

    Delete src directory

--- src/value_updater.cpp
+++
@@ -1,7 +0,0 @@
-// Writes updated values, such as chunk lengths, CRC, etc. into the relevant vector index locations.
-
-void valueUpdater(std::vector<uchar>& vec, uint32_t value_insert_index, const uint32_t NEW_VALUE, uint8_t value_bit_length, bool isBigEndian) {
-	while (value_bit_length) {
-		static_cast<uint32_t>(vec[isBigEndian ? value_insert_index++ : value_insert_index--] = (NEW_VALUE >> (value_bit_length -= 8)) & 0xff);
-	}
-}
