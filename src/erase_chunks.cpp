df21aea3caefadcfef6d74b535a53a9afe302ec2
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-23 11:57:16 +0100

    Add files via upload

---
+++ src/erase_chunks.cpp
@@ -0,0 +1,57 @@
+// Erase chunks from cover image. Just keep the critical PNG chunks.
+
+uint_fast32_t eraseChunks(std::vector<uint_fast8_t>& Image_Vec, uint_fast32_t image_size) {
+	
+	std::vector<uint_fast8_t>Temp_Vec;
+
+	constexpr uint_fast8_t 
+		PNG_HEADER_IHDR_CHUNK = 0x21,
+		PNG_IEND_LENGTH = 12,
+		INDEXED_COLOR_TYPE = 3,
+		PLTE_SIG[4]  { 0x50, 0x4C, 0x54, 0x45 },
+		IDAT_SIG [4] { 0x49, 0x44, 0x41, 0x54 };
+
+	Temp_Vec.insert(Temp_Vec.begin(), Image_Vec.begin(), Image_Vec.begin() + PNG_HEADER_IHDR_CHUNK);
+
+	uint_fast32_t
+		idat_chunk_index = static_cast<uint_fast32_t>(std::search(Image_Vec.begin(), Image_Vec.end(), std::begin(IDAT_SIG), std::end(IDAT_SIG)) - Image_Vec.begin() - 4),
+		buf_index{},
+		initialize_crc_value = 0xffffffffL;
+
+	const uint_fast32_t
+		FIRST_IDAT_CHUNK_LENGTH = getFourByteValue(Image_Vec, idat_chunk_index),
+		FIRST_IDAT_CHUNK_CRC_INDEX = idat_chunk_index + FIRST_IDAT_CHUNK_LENGTH + 8,
+		FIRST_IDAT_CHUNK_CRC = getFourByteValue(Image_Vec, FIRST_IDAT_CHUNK_CRC_INDEX),
+		CALC_FIRST_IDAT_CHUNK_CRC = crcUpdate(&Image_Vec[idat_chunk_index + 4], FIRST_IDAT_CHUNK_LENGTH + 4, buf_index, initialize_crc_value);
+
+	if (FIRST_IDAT_CHUNK_CRC != CALC_FIRST_IDAT_CHUNK_CRC) {
+		std::cerr << "\nImage File Error: CRC value for first IDAT chunk is invalid.\n\n";
+		std::exit(EXIT_FAILURE);
+	}
+
+	const uint_fast8_t IMAGE_COLOR_TYPE = Image_Vec[25];
+
+	if (IMAGE_COLOR_TYPE == INDEXED_COLOR_TYPE) {
+
+		const uint_fast32_t PLTE_CHUNK_INDEX = static_cast<uint_fast32_t>(std::search(Image_Vec.begin(), Image_Vec.end(), std::begin(PLTE_SIG), std::end(PLTE_SIG)) - Image_Vec.begin() - 4);
+
+		if (idat_chunk_index > PLTE_CHUNK_INDEX) {
+			const uint_fast32_t PLTE_CHUNK_LENGTH = getFourByteValue(Image_Vec, PLTE_CHUNK_INDEX);
+			Temp_Vec.insert(Temp_Vec.end(), Image_Vec.begin() + PLTE_CHUNK_INDEX, Image_Vec.begin() + PLTE_CHUNK_INDEX + (PLTE_CHUNK_LENGTH + 12));
+		} else {
+			std::cerr << "\nImage File Error: Required PLTE chunk not found for PNG-8 Indexed-color image.\n\n";
+			std::exit(EXIT_FAILURE);
+		}
+	}
+
+	while (image_size != idat_chunk_index + 4) {
+		const uint_fast32_t IDAT_CHUNK_LENGTH = getFourByteValue(Image_Vec, idat_chunk_index);
+		Temp_Vec.insert(Temp_Vec.end(), Image_Vec.begin() + idat_chunk_index, Image_Vec.begin() + idat_chunk_index + (IDAT_CHUNK_LENGTH + 12));
+		idat_chunk_index = static_cast<uint_fast32_t>(std::search(Image_Vec.begin() + idat_chunk_index + 6, Image_Vec.end(), std::begin(IDAT_SIG), std::end(IDAT_SIG)) - Image_Vec.begin() - 4);
+	}
+
+	Temp_Vec.insert(Temp_Vec.end(), Image_Vec.end() - PNG_IEND_LENGTH, Image_Vec.end());
+	Temp_Vec.swap(Image_Vec);
+
+	return static_cast<uint_fast32_t>(Image_Vec.size());
+}
---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+// 	PNG Data Vehicle, ZIP Edition (PDVZIP v2.3.4). 
+//	Created by Nicholas Cleasby (@CleasbyCode) 6/08/2022
+
+//	To compile program (Linux):
+// 	$ g++ main.cpp -O2 -s -o pdvzip
+
+// 	Run it:
+// 	$ ./pdvzip
+
+#include "pdvzip.h"
+
+int main(int argc, char** argv) {
+	
+	if (argc == 2 && std::string(argv[1]) == "--info") {
+		displayInfo();
+	} else if (argc < 3 || argc > 3) {
+		std::cout << "\nUsage: pdvzip <png> <zip/jar>\n\t\bpdvzip --info\n\n";
+	} else {
+		
+		bool isZipFile = true;
+
+		std::string
+			IMAGE_NAME = argv[1],
+			ZIP_NAME = argv[2];
+
+		const std::regex REG_EXP("(\\.[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+)?[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+?(\\.[a-zA-Z0-9]+)?");
+		
+		const std::string
+			GET_PNG_EXT = IMAGE_NAME.length() > 2 ? IMAGE_NAME.substr(IMAGE_NAME.length() - 3) : IMAGE_NAME,
+			GET_ZIP_EXT = ZIP_NAME.length() > 2 ? ZIP_NAME.substr(ZIP_NAME.length() - 3) : ZIP_NAME;
+
+		if (GET_PNG_EXT != "png" || GET_ZIP_EXT != "zip" && GET_ZIP_EXT != "jar" || !regex_match(IMAGE_NAME, REG_EXP) || !regex_match(ZIP_NAME, REG_EXP)) {
+			std::cerr << (GET_PNG_EXT != "png" || GET_ZIP_EXT != "zip" ? 
+			"\nFile Type Error: Invalid file extension found. Expecting only '.png' followed by '.zip/.jar'"
+			: "\nInvalid Input Error: Characters not supported by this program found within file name arguments") << ".\n\n";
+			std::exit(EXIT_FAILURE);
+		}
+
+		isZipFile = GET_ZIP_EXT == "jar" ? false : isZipFile;
+		
+		startPdv(IMAGE_NAME, ZIP_NAME, isZipFile);
+	}
+	return 0;
+}
