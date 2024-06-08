6e55f7d7bf38d2de93a5f8e21976c89d2941a252
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-06-08 16:25:16 +0100

    Update adjustZip.cpp

--- src/adjustZip.cpp
+++ src/adjustZip.cpp
@@ -2,9 +2,9 @@
 void adjustZipOffsets(std::vector<uint_fast8_t>& Image_Vec, const uint_fast32_t LAST_IDAT_CHUNK_NAME_INDEX) {
  
 	constexpr uint_fast8_t 
-		ZIP_SIG[] 			{0x50, 0x4B, 0x03, 0x04},
-		START_CENTRAL_DIR_SIG [] 	{0x50, 0x4B, 0x01, 0x02},
-		END_CENTRAL_DIR_SIG [] 		{0x50, 0x4B, 0x05, 0x06},
+		ZIP_SIG[] 		{ 0x50, 0x4B, 0x03, 0x04 },
+		START_CENTRAL_DIR_SIG[] { 0x50, 0x4B, 0x01, 0x02 },
+		END_CENTRAL_DIR_SIG[] 	{ 0x50, 0x4B, 0x05, 0x06 },
 		PNG_IEND_LENGTH = 16;
 
 	const uint_fast32_t
