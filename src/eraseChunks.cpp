5a27b46f4522b76f68416044348d6795ac1137df
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-07-27 17:46:09 +0100

    Update eraseChunks.cpp

--- src/eraseChunks.cpp
+++ src/eraseChunks.cpp
@@ -1,10 +1,9 @@
 // Erase superfluous PNG chunks from the cover image. Just keep the critical PNG chunks/data. (HEADER/IHDR/*PLTE/IDAT/IEND).
-uint_fast32_t eraseChunks(std::vector<uint_fast8_t>& Image_Vec) {
+uint32_t eraseChunks(std::vector<uint8_t>& Image_Vec) {
 	
-	std::vector<uint_fast8_t>Temp_Vec;
-	Temp_Vec.reserve(Image_Vec.size());
+	std::vector<uint8_t>Temp_Vec;
 
-	constexpr uint_fast8_t 
+	constexpr uint8_t 
 		PNG_HEADER_IHDR_CHUNK_LENGTH = 33,
 		PNG_IEND_CHUNK_LENGTH = 12,
 		INDEXED_COLOR_TYPE = 3,
@@ -14,9 +13,9 @@ uint_fast32_t eraseChunks(std::vector<uint_fast8_t>& Image_Vec) {
 
 	Temp_Vec.insert(Temp_Vec.begin(), Image_Vec.begin(), Image_Vec.begin() + PNG_HEADER_IHDR_CHUNK_LENGTH);
 
-	uint_fast32_t idat_chunk_index = searchFunc(Image_Vec, 0, 0, IDAT_SIG) - 4;
+	uint32_t idat_chunk_index = searchFunc(Image_Vec, 0, 0, IDAT_SIG) - 4;
 
-	const uint_fast32_t
+	const uint32_t
 		IMAGE_FILE_SIZE = getVecSize(Image_Vec),
 		FIRST_IDAT_CHUNK_LENGTH = getFourByteValue(Image_Vec, idat_chunk_index),
 		FIRST_IDAT_CHUNK_CRC_INDEX = idat_chunk_index + FIRST_IDAT_CHUNK_LENGTH + 8,
@@ -28,12 +27,12 @@ uint_fast32_t eraseChunks(std::vector<uint_fast8_t>& Image_Vec) {
 		std::exit(EXIT_FAILURE);
 	}
 
-	const uint_fast8_t IMAGE_COLOR_TYPE = Image_Vec[25];
+	const uint8_t IMAGE_COLOR_TYPE = Image_Vec[25];
 
 	if (IMAGE_COLOR_TYPE == INDEXED_COLOR_TYPE) {
-		const uint_fast32_t PLTE_CHUNK_INDEX = searchFunc(Image_Vec, 0, 0, PLTE_SIG) - 4;
+		const uint32_t PLTE_CHUNK_INDEX = searchFunc(Image_Vec, 0, 0, PLTE_SIG) - 4;
 		if (idat_chunk_index > PLTE_CHUNK_INDEX) {
-			const uint_fast32_t PLTE_CHUNK_LENGTH = getFourByteValue(Image_Vec, PLTE_CHUNK_INDEX);
+			const uint32_t PLTE_CHUNK_LENGTH = getFourByteValue(Image_Vec, PLTE_CHUNK_INDEX);
 			Temp_Vec.insert(Temp_Vec.end(), Image_Vec.begin() + PLTE_CHUNK_INDEX, Image_Vec.begin() + PLTE_CHUNK_INDEX + (PLTE_CHUNK_LENGTH + 12));
 		} else {
 		    std::cerr << "\nImage File Error: Required PLTE chunk not found for PNG-8 Indexed-color image.\n\n";
@@ -42,7 +41,7 @@ uint_fast32_t eraseChunks(std::vector<uint_fast8_t>& Image_Vec) {
 	}
 
 	while (IMAGE_FILE_SIZE != idat_chunk_index + 4) {
-		uint_fast32_t idat_chunk_length = getFourByteValue(Image_Vec, idat_chunk_index);
+		uint32_t idat_chunk_length = getFourByteValue(Image_Vec, idat_chunk_index);
 		Temp_Vec.insert(Temp_Vec.end(), Image_Vec.begin() + idat_chunk_index, Image_Vec.begin() + idat_chunk_index + (idat_chunk_length + 12));
 		idat_chunk_index = searchFunc(Image_Vec, idat_chunk_index, 6, IDAT_SIG) - 4;
 	}
