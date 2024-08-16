6fcb035c2b9146d7a8acb75934dc197176e429f5
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-08-16 23:09:28 +0100

    Update checkIhdrChunk.cpp

--- src/checkIhdrChunk.cpp
+++ src/checkIhdrChunk.cpp
@@ -1,11 +1,10 @@
 // A range of characters that may appear within the image width/height dimension fields of the IHDR chunk or within the 4-byte IHDR chunk's CRC field,
 // will break the Linux extraction script. If any of these characters are detected, the user will have to modify the image manually or try another image.
 void checkIhdrChunk(std::vector<uint8_t>& Vec) {
-
 	constexpr uint8_t
 			LINUX_PROBLEM_CHARACTERS[] { 0x22, 0x27, 0x28, 0x29, 0x3B, 0x3E, 0x60 },
 			MAX_INDEX = 0x20;
-	
+						    
 	uint8_t ihdr_chunk_index = 0x12;
 
 	while (ihdr_chunk_index++ != MAX_INDEX) {
