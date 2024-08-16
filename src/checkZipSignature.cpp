67e56434d74163c54b01a2ae7869b7f179ff0b3a
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-08-16 23:12:39 +0100

    Update checkZipSignature.cpp

--- src/checkZipSignature.cpp
+++ src/checkZipSignature.cpp
@@ -1,5 +1,4 @@
 void checkZipSignature(std::vector<uint8_t>& Vec) {
-
 	constexpr uint8_t ZIP_SIG[] { 0x50, 0x4B, 0x03, 0x04 };
 
 	if (!std::equal(std::begin(ZIP_SIG), std::end(ZIP_SIG), std::begin(Vec) + 8)) {
