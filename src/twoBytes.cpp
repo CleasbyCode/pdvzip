ee44c5b46c770fc6eede08b02250270537050629
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-08-16 23:03:59 +0100

    Delete src directory

--- src/twoBytes.cpp
+++
@@ -1,5 +0,0 @@
-uint16_t getTwoByteValue(const std::vector<uint8_t>& VEC, uint32_t index, bool isBigEndian) {
-	return (static_cast<uint16_t>(VEC[index]) << 8) | 
-		static_cast<uint16_t>(VEC[isBigEndian ? ++index : --index]);
-}
-
