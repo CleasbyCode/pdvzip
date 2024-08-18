729a717d7891e4823c8c5a025350ec82a11a2cbc
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-08-18 19:43:33 +0100

    Update adjustZip.cpp

--- src/adjustZip.cpp
+++ src/adjustZip.cpp
@@ -1,6 +1,5 @@
 // ZIP file has been moved to another location. We need to find and adjust the ZIP file record offsets to their new location.
 void adjustZipOffsets(std::vector<uint8_t>& Vec, const uint32_t LAST_IDAT_CHUNK_NAME_INDEX) {
- 
 	constexpr uint8_t 
 		ZIP_SIG[] 		{ 0x50, 0x4B, 0x03, 0x04 },
 		START_CENTRAL_DIR_SIG[] { 0x50, 0x4B, 0x01, 0x02 },
@@ -34,6 +33,5 @@ void adjustZipOffsets(std::vector<uint8_t>& Vec, const uint32_t LAST_IDAT_CHUNK_
 		central_local_index = 0x2D + searchFunc(Vec, central_local_index, 0, START_CENTRAL_DIR_SIG);
 		valueUpdater(Vec, central_local_index, new_offset, value_bit_length, false);
 	}
-
 	valueUpdater(Vec, end_central_start_index, START_CENTRAL_DIR_INDEX, value_bit_length, false);
 }
