7a25babcecb46b55804f2e373984f519eac04cf4
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-21 20:16:58 +0100

    Update adjustZip.cpp

--- src/adjustZip.cpp
+++ src/adjustZip.cpp
@@ -1,5 +1,10 @@
 // ZIP file has been moved to another location. We need to find and adjust the ZIP file record offsets to their new location.
 void adjustZipOffsets(std::vector<uint8_t>& Vec, const uint_fast32_t VEC_SIZE, const uint_fast32_t LAST_IDAT_INDEX) {
+
+	auto valueUpdater = [](std::vector<uint8_t>& Vec, uint_fast32_t value_insert_index, const uint_fast32_t NEW_VALUE, uint_fast8_t bits) {
+		while (bits) { Vec[value_insert_index--] = (NEW_VALUE >> (bits -= 8)) & 0xff; }
+	};
+
 	constexpr uint_fast8_t 
 		ZIP_SIG[] 			{ 0x50, 0x4B, 0x03, 0x04 },
 		START_CENTRAL_DIR_SIG[] 	{ 0x50, 0x4B, 0x01, 0x02 },
@@ -29,8 +34,7 @@ void adjustZipOffsets(std::vector<uint8_t>& Vec, const uint_fast32_t VEC_SIZE, c
 		record_count = 0;
 	
 	uint_fast8_t value_bit_length = 16;
-
-	valueUpdater(Vec, zip_comment_length_index, zip_comment_length, value_bit_length, false); // Update extended comment length value. 
+	valueUpdater(Vec, zip_comment_length_index, zip_comment_length, value_bit_length); // Update extended comment length value. 
 		
 	// Find the first start central directory index location by reverse iterating over the vector, starting from the end of the ZIP/IMAGE contents.
 	// Seems safer than searching from the start of the ZIP, as there's a chance of a false SIG match for start_central_dir, especially with large files.
@@ -44,11 +48,10 @@ void adjustZipOffsets(std::vector<uint8_t>& Vec, const uint_fast32_t VEC_SIZE, c
 	uint_fast32_t central_dir_local_offset_index = start_central_dir_index + CENTRAL_LOCAL_INDEX_DIFF;
 
 	value_bit_length = 32;
-
-	valueUpdater(Vec, end_central_start_index, start_central_dir_index, value_bit_length, false);
+	valueUpdater(Vec, end_central_start_index, start_central_dir_index, value_bit_length);
 	
 	while (total_zip_records--) {
-		valueUpdater(Vec, central_dir_local_offset_index, zip_record_offset_index, value_bit_length, false);
+		valueUpdater(Vec, central_dir_local_offset_index, zip_record_offset_index, value_bit_length);
 		zip_record_offset_index = searchFunc(Vec, zip_record_offset_index, INCREMENT_SEARCH_INDEX, ZIP_SIG);
 		central_dir_local_offset_index = searchFunc(Vec, central_dir_local_offset_index, INCREMENT_SEARCH_INDEX, START_CENTRAL_DIR_SIG) + CENTRAL_LOCAL_INDEX_DIFF;
 	}
