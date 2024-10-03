397ed9c833ea353b83275ca0bb8b6dbff928f0c7
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-10-03 17:07:30 +0100

    Update eraseChunks.cpp

--- src/eraseChunks.cpp
+++ src/eraseChunks.cpp
@@ -1,7 +1,6 @@
 // Erase superfluous PNG chunks from the cover image. Just keep the critical PNG chunks/data. (HEADER/IHDR/*PLTE/IDAT/IEND).
 void eraseChunks(std::vector<uint8_t>& Vec) {
-
-	constexpr uint_fast8_t 
+	constexpr uint8_t 
 		PLTE_SIG[] 	{ 0x50, 0x4C, 0x54, 0x45 },
 		IDAT_SIG[] 	{ 0x49, 0x44, 0x41, 0x54 },
 		PNG_COLOR_TYPE_INDEX 		= 0x19,
@@ -18,20 +17,20 @@ void eraseChunks(std::vector<uint8_t>& Vec) {
 	// Copy the first 33 bytes of the cover image into Temp_Vec. This contains the PNG header along with the IHDR chunk.
 	Temp_Vec.insert(Temp_Vec.begin(), Vec.begin(), Vec.begin() + PNG_FIRST_BYTES);
 
-	uint_fast32_t 
+	uint32_t 
 		idat_chunk_length_index = 0,	
 		idat_chunk_name_index = 0,
 		idat_chunk_length = 0;
 
 	idat_chunk_name_index = searchFunc(Vec, idat_chunk_name_index, INCREMENT_SEARCH_INDEX, IDAT_SIG); // Search for first IDAT chunk.
 
-	const uint_fast8_t IMAGE_COLOR_TYPE = Vec[PNG_COLOR_TYPE_INDEX];
+	const uint8_t IMAGE_COLOR_TYPE = Vec[PNG_COLOR_TYPE_INDEX];
 
 	// If required, search for and copy the PLTE chunk from cover image into Temp_Vec.
 	if (IMAGE_COLOR_TYPE == INDEXED_COLOR) {
-		uint_fast32_t plte_chunk_name_index = 0;
+		uint32_t plte_chunk_name_index = 0;
 		plte_chunk_name_index = searchFunc(Vec, plte_chunk_name_index, INCREMENT_SEARCH_INDEX, PLTE_SIG);
-		const uint_fast32_t 
+		const uint32_t 
 			PLTE_CHUNK_LENGTH_INDEX = plte_chunk_name_index - 4,
 			PLTE_CHUNK_LENGTH = getByteValue(Vec, PLTE_CHUNK_LENGTH_INDEX, BYTE_LENGTH, false) + LENGTH_NAME_CRC_FIELD_BYTES;
 		Temp_Vec.insert(Temp_Vec.end(), Vec.begin() + PLTE_CHUNK_LENGTH_INDEX, Vec.begin() + PLTE_CHUNK_LENGTH_INDEX + PLTE_CHUNK_LENGTH);
