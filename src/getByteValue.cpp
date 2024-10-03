a6261244052e7e679297a018579d511a1b99a0d5
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-10-03 17:09:58 +0100

    Update getByteValue.cpp

--- src/getByteValue.cpp
+++ src/getByteValue.cpp
@@ -1,19 +1,15 @@
 // Return either a 4-byte or 2-byte value from a given vector index position.
-uint_fast32_t getByteValue(const std::vector<uint8_t>& VEC, const uint_fast32_t INDEX, const uint_fast8_t BYTE_LENGTH, bool isBigEndian) {
-	
+uint32_t getByteValue(const std::vector<uint8_t>& VEC, const uint32_t INDEX, const uint8_t BYTE_LENGTH, bool isBigEndian) {
 	if (BYTE_LENGTH == 4) {
-	
-	return	(static_cast<uint_fast32_t>(VEC[INDEX]) << 24) |
-		(static_cast<uint_fast32_t>(VEC[INDEX + 1]) << 16) |
-		(static_cast<uint_fast32_t>(VEC[INDEX + 2]) << 8) |
-		 static_cast<uint_fast32_t>(VEC[INDEX + 3]); 
+		return	(static_cast<uint32_t>(VEC[INDEX]) << 24) |
+			(static_cast<uint32_t>(VEC[INDEX + 1]) << 16) |
+			(static_cast<uint32_t>(VEC[INDEX + 2]) << 8) |
+		 	static_cast<uint32_t>(VEC[INDEX + 3]); 
 	} else {
 		if (isBigEndian) {
-       		 return (static_cast<uint_fast16_t>(VEC[INDEX]) << 8) | static_cast<uint_fast16_t>(VEC[INDEX + 1]);
-    		} else {
-        	  return (static_cast<uint_fast16_t>(VEC[INDEX]) << 8) | static_cast<uint_fast16_t>(VEC[INDEX - 1]);
-    		}
-		
+			return (static_cast<uint16_t>(VEC[INDEX]) << 8) | static_cast<uint16_t>(VEC[INDEX + 1]);
+		} else {
+			return (static_cast<uint16_t>(VEC[INDEX]) << 8) | static_cast<uint16_t>(VEC[INDEX - 1]);
+		}
 	}
-
 }
