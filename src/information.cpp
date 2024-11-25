aba62e5ea4d26099f357d4c725e84b41f5c9b92c
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-11-25 09:08:41 +0000

    Update information.cpp

--- src/information.cpp
+++ src/information.cpp
@@ -1,6 +1,6 @@
 void displayInfo() {
 	std::cout << R"(
-PNG Data Vehicle ZIP Edition (PDVZIP v2.5). 
+PNG Data Vehicle ZIP Edition (PDVZIP v2.6). 
 Created by Nicholas Cleasby (@CleasbyCode) 6/08/2022.
 
 Use PDVZIP to embed a ZIP/JAR file within a PNG image, 
