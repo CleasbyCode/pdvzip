fdf3a8a7be9847ae36dce8ae048ddae50336ed02
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-12-06 02:35:13 +0000

    Update resizeImage.cpp

--- src/resizeImage.cpp
+++ src/resizeImage.cpp
@@ -1,5 +1,5 @@
-// Using lodepng, resize the cover image within the vector, by reducing it's width & height by 1 pixel. 
-// https://github.com/lvandeve/lodepng  (Copyright (c) 2005-2024 Lode Vandevenne)
+// Using lodepng to resize the cover image within the vector, by reducing it's width & height by 1 pixel. 
+// https://github.com/lvandeve/lodepng  (Copyright (c) 2005-2024 Lode Vandevenne).
 
 void resizeImage(std::vector<uint8_t>& Image_Vec) {
     std::vector<uint8_t> Temp_Vec; 
