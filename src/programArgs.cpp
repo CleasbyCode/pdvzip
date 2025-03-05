6819241e403be2d1c47a1370a8fbeee5cce036f5
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-03-05 20:23:40 +0000

    Update programArgs.cpp

--- src/programArgs.cpp
+++ src/programArgs.cpp
@@ -21,13 +21,14 @@ ProgramArgs ProgramArgs::parse(int argc, char** argv) {
 	if (argc != 3) {
         	throw std::runtime_error("Usage: pdvzip <cover_image> <zip/jar>\n\t\bpdvzip --info");
     	}
-	std::filesystem::path archive_path(args.archive_file);
+	
+    	args.image_file = argv[1];
+    	args.archive_file = argv[2];
 
+	std::filesystem::path archive_path(args.archive_file);
 	if (archive_path.extension().string() == ".jar") {
 		args.thisArchiveType = ArchiveType::JAR;
 	}
-
-    	args.image_file = argv[1];
-    	args.archive_file = argv[2];
+	
     	return args;
 }
