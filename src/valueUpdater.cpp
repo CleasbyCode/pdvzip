3ba7b886172e38f217d7902e328048136100296b
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-05-16 19:07:46 +0100

    Delete src/valueUpdater.cpp

--- src/valueUpdater.cpp
+++
@@ -1,6 +0,0 @@
-// Writes updated values, such as chunk lengths, CRC, etc. into the relevant vector index locations.
-void valueUpdater(std::vector<uint8_t>& vec, uint32_t value_index, const uint32_t NEW_VALUE, uint8_t value_bit_length) {
-	while (value_bit_length) {
-		vec[value_index++] = (NEW_VALUE >> (value_bit_length -= 8)) & 0xff;
-	}
-}
