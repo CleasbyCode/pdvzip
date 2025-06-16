78a9cbb01936b10dd4f3cf7202a92b2e0584d90e
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-06-16 18:55:14 +0100

    Update adjustZip.cpp

--- src/adjustZip.cpp
+++ src/adjustZip.cpp
@@ -1,22 +1,17 @@
 // ZIP file has moved to another location. We need to find and adjust the ZIP file record offsets to their new location.
 #include "adjustZip.h"
 #include "searchFunc.h"
+#include <cstring>
 #include <iostream>
 #include <iterator>         
 
 void adjustZipOffsets(std::vector<uint8_t>& vec, const uint32_t VEC_SIZE, const uint32_t LAST_IDAT_INDEX) {	
-	// Small Lambda function used multiple times in this function. 
-	// Writes updated values (2 bytes or 4 bytes), such as chunk lengths, index/offsets, CRC, etc. into the relevant vector index location.	
-	auto updateValue = [](std::vector<uint8_t>& vec, uint32_t insert_index, const uint32_t NEW_VALUE, uint8_t bits) {
-		while (bits) { vec[insert_index--] = (NEW_VALUE >> (bits -= 8)) & 0xff; }	// Little-endian for ZIP.
-	};
-
 	constexpr uint8_t
-		CENTRAL_LOCAL_INDEX_DIFF 	= 45,
+		CENTRAL_LOCAL_INDEX_DIFF 	= 42, 
 		ZIP_COMMENT_LENGTH_INDEX_DIFF 	= 21,
-		END_CENTRAL_START_INDEX_DIFF 	= 19,
-		ZIP_RECORDS_INDEX_DIFF 		= 11,
+		END_CENTRAL_START_INDEX_DIFF 	= 16, 
 		PNG_IEND_LENGTH 		= 16,
+		ZIP_RECORDS_INDEX_DIFF 		= 11,
 		ZIP_LOCAL_INDEX_DIFF 		= 4,
 		SIG_LENGTH 			= 4,
 		INCREMENT_NEXT_SEARCH_POS	= 1;
@@ -37,13 +32,11 @@ void adjustZipOffsets(std::vector<uint8_t>& vec, const uint32_t VEC_SIZE, const 
 		start_central_dir_index  = 0; 
 
 	uint16_t 
-		total_zip_records = (vec[total_zip_records_index] << 8) | vec[total_zip_records_index - 1],
-		zip_comment_length = ((vec[zip_comment_length_index] << 8) | vec[zip_comment_length_index - 1]) + PNG_IEND_LENGTH, // Extend comment length. Includes end bytes of PNG. Required for JAR.
+		total_zip_records = (vec[total_zip_records_index] << 8) | vec[--total_zip_records_index],
+		zip_comment_length = ((vec[zip_comment_length_index] << 8) | vec[--zip_comment_length_index]) + PNG_IEND_LENGTH, // Extend comment length. Includes end bytes of PNG. Required for JAR.
 		record_count = 0;
-						 
-	uint8_t value_bit_length = 16;
-	
-	updateValue(vec, zip_comment_length_index, zip_comment_length, value_bit_length); 
+
+	std::memcpy(&vec[zip_comment_length_index], &zip_comment_length, sizeof(uint16_t));
 
 	// Find the first, top "start_central directory" index location by searching the vector, working backwards from the vector's content.
 	// By starting the search from the end of the vector, we know we are already within the record section data of the archive and this helps
@@ -57,11 +50,10 @@ void adjustZipOffsets(std::vector<uint8_t>& vec, const uint32_t VEC_SIZE, const 
 	
 	uint32_t central_dir_local_index = start_central_dir_index + CENTRAL_LOCAL_INDEX_DIFF; // First central dir index location we need to update with the new ZIP record index/offset.
 
-	value_bit_length = 32;
-	updateValue(vec, end_central_start_index, start_central_dir_index, value_bit_length);  // Update end_central index location with start_central directory offset.
+	std::memcpy(&vec[end_central_start_index], &start_central_dir_index, sizeof(uint32_t));
 	
 	while (total_zip_records--) {
-		updateValue(vec, central_dir_local_index, zip_record_local_index, value_bit_length); // Write the new zip_record index/offset to the current central_dir_local index location.
+		std::memcpy(&vec[central_dir_local_index], &zip_record_local_index, sizeof(uint32_t));
 		zip_record_local_index  = searchFunc(vec, zip_record_local_index, INCREMENT_NEXT_SEARCH_POS, ZIP_LOCAL_SIG); // Get the next zip_record index/offset.
 		central_dir_local_index = searchFunc(vec, central_dir_local_index, INCREMENT_NEXT_SEARCH_POS, START_CENTRAL_DIR_SIG) + CENTRAL_LOCAL_INDEX_DIFF; // Get the next central_dir index location.
 	}
