c365f7f2e03cd4428f940dc504fbcd109e9c3de5
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-12-11 16:51:20 +0000

    Update compile_pdvzip.sh

--- src/compile_pdvzip.sh
+++ src/compile_pdvzip.sh
@@ -2,7 +2,7 @@
 
 # compile_pdvzip.sh
 
-g++ -std=c++20 pdvzip.cpp lodepng/lodepng.cpp -Wall -O3 -s -o pdvzip
+g++ -std=c++23 -O3 -march=native -pipe -Wall -Wextra -Wpedantic -DNDEBUG -s -flto=auto -fuse-linker-plugin pdvzip.cpp lodepng/lodepng.cpp -o pdvzip
 
 if [ $? -eq 0 ]; then
     echo "Compilation successful. Executable 'pdvzip' created."
