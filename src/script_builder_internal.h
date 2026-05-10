31d5bb09ddee38a5cb979e860d941c7681b1c7dd
Nicholas Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2026-05-10 13:41:13 +0100

    Delete src directory

--- src/script_builder_internal.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "pdvzip.h"
-
-namespace script_builder_internal {
-
-std::string buildScriptText(FileType file_type, const std::string& first_filename, const UserArguments& user_args);
-
-}  // namespace script_builder_internal
